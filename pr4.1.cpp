ffa385097552abfab23ed2c953770e5d39ad3b6e
Andrew <90443266+Andrew2865@users.noreply.github.com>
2021-10-20 23:54:12 +0300

    Add files via upload

---
+++ pr4.1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k, N, i;
+	double S;
+	cout << "k = "; cin >> k;
+	cout << "N = "; cin >> N;
+	S = 0;
+	i = k;
+	while (i <= N)
+	{
+		S += (sin(10* i)*cos(10/i))/sqrt(i);
+		i++;
+	}
+	cout << S << endl;
+		S = 0;
+	i = k;
+	do {
+		S += (sin(10 * i) * cos(10 / i)) / sqrt(i);
+		i++;
+	} while (i <= N);
+	cout << S << endl;
+	S = 0;
+	for (i = k; i <= N; i++)
+	{
+		S += (sin(10 * i) * cos(10 / i)) / sqrt(i);
+	}
+	cout << S << endl;
+	S = 0;
+	for (i = N; i >= k; i--)
+	{
+		S += (sin(10 * i) * cos(10 / i)) / sqrt(i);
+	}
+	cout << S << endl;
+	return 0;
+}
---
+++ pr4.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx,a,b,c, F;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{ 
+		if (a < 0 && x != 0)
+			F = a * pow(x, 2) + pow(b, 2) * x;
+		if (a > 0 && x = 0)
+			F = x - (a / (x - c));
+		else
+			F = 1 + (x / c);
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
