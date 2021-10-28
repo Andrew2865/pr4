a758660176429b731b034e16bf5a623be652e7b9
Andrew <90443266+Andrew2865@users.noreply.github.com>
2021-10-28 08:56:24 +0300

    Add files via upload

---
+++ pr4.5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y,R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		if ((y <= (x - 1) * (x - 1) && y >= R) ||
+			(y >= R && y <=sqrt( (x * x) - (R * R))))
+				cout << "yes" << endl;
+		else
+				cout << "no" << endl;
+	}
+
+	cout << endl << fixed;
+
+
+		for (int i = 0; i < 10; i++)
+		{
+			x = 6. * rand() / RAND_MAX - 3;
+			y = 6. * rand() / RAND_MAX - 3;
+			if ((y <= (x - 1) * (x - 1) && y >= R) ||
+				(y >= R && y <= sqrt((x * x) - (R * R))))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+	return 0;
+}
---
+++ pr4.6.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	double P, S;
+	int j, i;
+	S = 0;
+	j = 2;
+	while (j <= 20)
+	{
+		S = 0;
+		j = 2;
+		while (i <= j)
+		{
+			S +=  j / (j ^ 2) + P;
+			i++;
+		}
+		P *= j;
+		j++;
+	}
+	cout << P << endl;
+	S = 0;
+	j = 2;
+	do
+	{
+		P = 1;
+		i = 1;
+		do
+		{
+			S += j / (j ^ 2) + P;
+			
+			i++;
+		} while (i <= j);
+		P *= j;
+		j++;
+	} while (j <= 20);
+	cout << S << endl;
+	S = 0;
+	for (j = 2; j <= 20; j++)
+	{
+		S = 0;
+		for (i = (j*j); i <= 400; i++)
+		{
+			S += j / (j ^ 2) + P;
+		}
+		P *= j;
+	}
+	cout << P << endl;
+	S = 0;
+	for (j = 20; j >= 1; j--
+		)
+
+	{
+		P = 1;
+		for (i = (j*j); i >= 400; i--
+			)
+
+		{
+			S += j / (j ^ 2) + P;;
+
+			
+
+		}
+		P *= j;
+	}
+	cout << P << endl;
+	return 0;
+}
